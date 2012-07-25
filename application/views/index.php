c8a013c79705c5b30fcfd261973b01edd115a73c
Mbugua <mbuguarufus@gmail.com>
2012-07-25 05:49:58 +0300

    Design edits

--- application/views/index.php
+++ application/views/index.php
@@ -3,58 +3,55 @@
 		<link href="<?php echo base_url(); ?>css/layout.css" rel="stylesheet" type="text/css" />
 		<!-- -->
 		<!-- Attach CSS files -->
-		<link rel="stylesheet" href="<?php echo base_url(); ?>css/layout-opt.css"/>
+		<link rel="stylesheet" href="<?php echo base_url(); ?>css/layout.css"/>
 		<link rel="stylesheet" href="<?php echo base_url(); ?>css/awesomebuttons.css"/>
 		<link rel="stylesheet" href="<?php echo base_url(); ?>css/buttons.css"/>
 		<link rel="stylesheet" href="<?php echo base_url(); ?>css/orbit.css"/>
 		<link rel="stylesheet" href="<?php echo base_url(); ?>css/post.css"/>
 		<link rel="stylesheet" href="<?php echo base_url(); ?>css/tabs.css"/>
-		<link rel="stylesheet" href="<?php echo base_url(); ?>css/form-opt.css"/>
+		<link rel="stylesheet" href="<?php echo base_url(); ?>css/form.css"/>
 		<!-- Attach JavaScript files -->
 		<script src="js/jquery-1.7.2.min.js" type="text/javascript"></script>
 		<script src="js/jquery.orbit.js" type="text/javascript"></script>
 	</head>
 	<body>
-		<section class="banner">
-			<section class="logo">
-				<img src="<?php echo base_url(); ?>images/coat_of_arms-resized.png" />
 
-			</section>
-			<section class="credentials">
-				<section class="title">
-					Ministry Of Public Health and Sanitation
-				</section>
-				<section class="subtitle">
-					Department of Family Health<p></p>
-				</section>
-				<section class="division">
-					Division of Nutrition<p></p><p></p>
-				</section>
-			</section>
-			<section class="date">
+		<header>
+			<section class="header-top">
+				<section class="date">
 
-				<?php echo date("l F d, Y"); ?>
+					<?php echo date("l F d, Y"); ?>
+				</section>
 			</section>
+			<section class="header-bottom">
+				<nav></nav>
+				<section class="search">
+					<form>
+						<input type="search" placeholder="Search Here"/>
+
+					</form>
+				</section>
+			</section>
+
+		</header>
+		<section class="content">
+			<section class="menu-container">
+				<section class="menu"></section>
+			</section>
+			<section class="tiles">
+				<section class="one">
+				<h2>Some Title</h2>
+			</section>
+			<section class="notifications">
+				<h2>Notifications</h2>
+			</section>
+			
+			<section class="two">
+				<h2>Some Title</h2>
+			</section>
+			</section>
+			
 		</section>
-
-		<section class="message">
-			<?php echo $form; ?>
-		</section>
-		<section class="login">
-			<section class="form-title">
-				<section class="title-text">
-					Sign In
-				</section>
-			</section>
-			<form class="form-login" method="post" accept-charset="utf-8">
-
-				<input name="username" type="text" placeholder="Username / Email Address"/>
-				<p></p>
-				<input  name="secret" type="password" placeholder="Password"/>
-				<p></p>
-				<button type="submit" class="awesome myblue large" formaction="<?php echo base_url().'c_auth/login'?>" />
-				Login</button>
-			</form>
-		</section>
+		<footer></footer>
 	</body>
 </html>
