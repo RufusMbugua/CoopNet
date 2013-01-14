2cbff2169b53f70394ea50adf80f1e2d147be3a5
Mbugua <mbuguarufus@gmail.com>
2013-01-14 04:54:18 +0300

    Minor Redesign

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://'.$_SERVER['SERVER_NAME'].'/CoffeeFarm/';
+$config['base_url']	= 'http://'.$_SERVER['SERVER_NAME'].'/CoopNet/';
 
 /*
 |--------------------------------------------------------------------------
--- application/views/index.php
+++ application/views/index.php
@@ -4,12 +4,6 @@
 		<!-- -->
 		<!-- Attach CSS files -->
 		<link rel="stylesheet" href="<?php echo base_url(); ?>css/layout.css"/>
-		<link rel="stylesheet" href="<?php echo base_url(); ?>css/awesomebuttons.css"/>
-		<link rel="stylesheet" href="<?php echo base_url(); ?>css/buttons.css"/>
-		<link rel="stylesheet" href="<?php echo base_url(); ?>css/orbit.css"/>
-		<link rel="stylesheet" href="<?php echo base_url(); ?>css/post.css"/>
-		<link rel="stylesheet" href="<?php echo base_url(); ?>css/tabs.css"/>
-		<link rel="stylesheet" href="<?php echo base_url(); ?>css/form.css"/>
 		<!-- Attach JavaScript files -->
 		<script src="js/jquery-1.7.2.min.js" type="text/javascript"></script>
 		<script src="js/jquery.orbit.js" type="text/javascript"></script>
@@ -19,42 +13,92 @@
 		<header>
 			<section class="header-top">
 				<section class="date">
-
 					<?php echo date("l F d, Y"); ?>
 				</section>
 			</section>
 			<section class="header-bottom">
-				<nav></nav>
+				<nav>
+					<ul>
+						<li>
+							<a href="#">Home</a>
+						</li>
+						<li>
+							<a href="#">Reports</a>
+						</li>
+					</ul>
+					
+				</nav>
+				
+
 				<section class="search">
 					<form>
 						<input type="search" placeholder="Search Here"/>
 
 					</form>
 				</section>
+				<div class="user">
+						Hi,User <a href="#">Logout</a>
+					</div>
 			</section>
 
 		</header>
 		<section class="content">
 			<section class="left-sidebar">
 				<section class="menu-container">
-				<section class="menu"></section>
+					<section class="menu">
+						<ul>
+							<li>
+								<a href="">Farmers</a>
+							</li>
+							<ul>
+								<li>
+									<a href="">Registration</a>
+								</li>
+								<li>
+									<a href="">Contracting</a>
+								</li>
+							</ul>
+							<li>
+								<a href="">Crops</a>
+							</li>
+							<ul>
+								<li>
+									<a href="">Coffee</a>
+								</li>
+								<ul>
+									<li>
+										<a href="">Buying</a>
+									</li>
+								</ul>
+							</ul>
+							<li>
+								<a href="">Departments</a>
+							</li>
+							<ul>
+
+							</ul>
+
+						</ul>
+					</section>
+				</section>
 			</section>
-			</section>
-			
+
 			<section class="tiles">
 				<section class="one">
-				<h3>Some Title</h3>
+					<h3>Some Title</h3>
+				</section>
+				<section class="notifications">
+					<h3>Notifications</h3>
+				</section>
+
+				<section class="two">
+					<h3>Some Title</h3>
+				</section>
 			</section>
-			<section class="notifications">
-				<h3>Notifications</h3>
-			</section>
-			
-			<section class="two">
-				<h3>Some Title</h3>
-			</section>
-			</section>
-			
+
 		</section>
-		<footer></footer>
+		<footer>
+			2012 Peace Kawomera
+		</footer>
 	</body>
 </html>
