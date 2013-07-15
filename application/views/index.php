02488c16b01e0e6c250043aea86298deb63324ca
unknown <mbui@.(none)>
2013-07-15 12:01:45 +0300

    Adjusted the content View

--- application/views/index.php
+++ application/views/index.php
@@ -1,41 +1,4 @@
-<section class="content">
-	<section class="left-sidebar well">
-		<section class="menu-container">
-			<section class="menu">
-				<ul class="nav nav-list">
-					<li>
-						<a href="">Farmers</a>
-					</li>
-					<ul class="nav nav-list">
-						<li>
-							<a href="">Registration</a>
-						</li>
-						<li>
-							<a href="">Contracting</a>
-						</li>
-					</ul>
-					<li>
-						<a href="">Crops</a>
-					</li>
-					<ul class="nav nav-list">
-						<li>
-							<a href="">Coffee</a>
-						</li>
-						<ul class="nav nav-list">
-							<li>
-								<a href="">Buying</a>
-							</li>
-						</ul>
-					</ul>
-					<li>
-						<a href="">Departments</a>
-					</li>
-					
 
-				</ul>
-			</section>
-		</section>
-	</section>
 
 	<section class="tiles">
 		<section class="one">
@@ -59,4 +22,4 @@
 		</section>
 	</section>
 
-</section>
+
--- application/views/template.php
+++ application/views/template.php
@@ -35,11 +35,49 @@
 			</section>
 
 		</header>
+		<section class="content">
+	<section class="left-sidebar well">
+		<section class="menu-container">
+			<section class="menu">
+				<ul class="nav nav-list">
+					<li>
+						<a href="">Farmers</a>
+					</li>
+					<ul class="nav nav-list">
+						<li>
+							<a href="">Registration</a>
+						</li>
+						<li>
+							<a href="">Contracting</a>
+						</li>
+					</ul>
+					<li>
+						<a href="">Crops</a>
+					</li>
+					<ul class="nav nav-list">
+						<li>
+							<a href="">Coffee</a>
+						</li>
+						<ul class="nav nav-list">
+							<li>
+								<a href="">Buying</a>
+							</li>
+						</ul>
+					</ul>
+					<li>
+						<a href="">Departments</a>
+					</li>
+					
+
+				</ul>
+			</section>
+		</section>
+	</section>
 		<?php
 
 		$this -> load -> view($contentView);
 		?>
-
+</section>
 		<footer class="well">
 			2012 Peace Kawomera
 		</footer>
